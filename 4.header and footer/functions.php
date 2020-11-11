21564bcd17aa9e96cb3afb4f723b308f3d22ee1a
davidv16 <davidv16@ru.is>
2020-11-11 18:25:35 +0000

    4. header and footer

---
+++ 4.header and footer/functions.php
@@ -0,0 +1,9 @@
+<?php
+
+function university_files() {
+  wp_enqueue_style('university_main_styles', get_stylesheet_uri());
+}
+
+add_action('wp_enqueue_scripts', 'university_files');
+
+?>
---
+++ header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  <?php wp_head(); ?>
+  </head>
+  <body>
+    <h1>Fictional University</h1>
+
