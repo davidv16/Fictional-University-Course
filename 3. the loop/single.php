eb1a314169005763450f3f6b496f47e42d672cf3
davidv16 <davidv16@ru.is>
2020-11-11 17:53:20 +0000

    3. the loop

--- 3. the loop/single.php
+++ 3. the loop/single.php
@@ -1,7 +1,7 @@
 <?php
   while(have_posts()) {
     the_post();?>
-    <h2><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h2>
+    <h2><?php the_title(); ?></h2>
     <?php the_content(); ?>
     <hr>
   <?php }
