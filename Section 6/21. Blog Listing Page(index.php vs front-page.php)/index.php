07c9c6b52b750891c2455af363f8885bb0dcdbe6
davidv16 <davidv16@ru.is>
2020-12-17 16:09:04 +0000

    section 6 complete

--- Section 6/21. Blog Listing Page(index.php vs front-page.php)/index.php
+++ Section 6/21. Blog Listing Page(index.php vs front-page.php)/index.php
@@ -20,7 +20,7 @@ get_header(); ?>
     <div class="post-item">
       <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink();?>"><?php the_title();?></a></h2>
 
-      <div class="metabox">
+      <div class="meta-box">
         <p> Posted by <?php the_author_posts_link();?> on <?php the_time('n.j.y');?> in <?php echo get_the_category_list(', ');?></p>
       </div>
 
@@ -30,9 +30,7 @@ get_header(); ?>
       </div>
     </div>
     <?php }
-    // for 1 2 next page links
-    echo paginate_links();
-?> 
+?>
 </div>
 
 <?php get_footer();
