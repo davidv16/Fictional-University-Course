659c2611f2fe17a217becfa429cab98aa429a4d3
davidv16 <davidv16@ru.is>
2021-01-20 23:30:21 +0000

    folder structure fixed

--- index.php
+++ index.php
@@ -1,34 +1,17 @@
 <?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
 
-get_header(); 
-pageBanner(array(
-  'title' => 'Welcome to my blog',
-  'subtitle' => 'Keep up with our latest news.'
-  ));
-?>
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define( 'WP_USE_THEMES', true );
 
-  <div class="container container--narrow page-section">
-<?php 
-// while posts exist, run the loop
-  while(have_posts()) {
-    the_post(); ?>
-    <div class="post-item">
-      <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink();?>"><?php the_title();?></a></h2>
-
-      <div class="metabox">
-        <p> Posted by <?php the_author_posts_link();?> on <?php the_time('n.j.y');?> in <?php echo get_the_category_list(', ');?></p>
-      </div>
-
-      <div class="generic-content">
-        <?php the_excerpt();?>
-        <p><a class="btn btn--blue" href="<?php the_permalink();?>"> Continue reading</a></p>
-      </div>
-    </div>
-    <?php }
-    // for 1 2 next page links
-    echo paginate_links();
-?> 
-</div>
-
-<?php get_footer();
-?>
+/** Loads the WordPress Environment and Template */
+require __DIR__ . '/wp-blog-header.php';
